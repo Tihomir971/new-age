0bed86cd2fa9fb799e9459dc4b327b1c5a064bf6
Tihomir971 <tihomir.jev@outlook.com>
2024-03-13 19:52:04 +0100

    add open props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+	class="max-w-content-3 grid gap-5 bg-surface-2 shadow-5 rounded-2xl p-8 hover:translate-y-5 ease-spring-3"
+>
+	<header>
+		<h1 class="text-5 text-ink-1">Remix | Tailwind | Open Props</h1>
+		<p class="text-ink-2">TWOP</p>
+	</header>
+	<ul>
+		<li>
+			<a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer" class="text-link">
+				15m Quickstart Blog Tutorial
+			</a>
+		</li>
+		<li>
+			<a
+				target="_blank"
+				href="https://remix.run/tutorials/jokes"
+				rel="noreferrer"
+				class="text-link"
+			>
+				Deep Dive Jokes App Tutorial
+			</a>
+		</li>
+		<li>
+			<a target="_blank" href="https://remix.run/docs" rel="noreferrer" class="text-link">
+				Remix Docs
+			</a>
+		</li>
+	</ul>
+</div>
